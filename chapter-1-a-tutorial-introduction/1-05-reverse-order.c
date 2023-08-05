7d433ba04c7aef811a5e9bd98a270948da2fa35c
halfrice <halfrice@protonmail.com>
2023-08-04 21:06:41 -0700

    Add Exercise 1-5

---
+++ chapter-1-a-tutorial-introduction/1-05-reverse-order.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+int main() {
+  float fahr, celsius;
+
+  printf("---------------------------\n");
+  printf("| Fahrenheit |    Celsius |\n");
+  printf("|------------+------------|\n");
+  for (fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("| %10.1f | %10.1f |\n", fahr, celsius);
+  }
+  printf("---------------------------\n");
+}
